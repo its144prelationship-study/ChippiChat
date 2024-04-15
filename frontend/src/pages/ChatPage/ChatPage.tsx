7779fcc53f81cafcb9726174d2c055aa69568e01
its144prelationship <pastalonely@gmail.com>
2024-04-16 03:48:54 +0700

    feat: init chat

--- frontend/src/pages/ChatPage/ChatPage.tsx
+++ frontend/src/pages/ChatPage/ChatPage.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Chat from "./components/Chat";
 import ChatLists from "./components/ChatLists";
+import Rings from "../../assets/rings.svg";
 
 export default function ChatPage() {
+  const [selectedChat, setSelectedChat] = useState("1");
   const [createGroup, setCreateGroup] = useState(false);
+  const [changeColor, setChangeColor] = useState(false);
+  const [chatColor, setChatColor] = useState("orange");
   const chatLists: ChatListType[] = [
     {
       id: "1",
@@ -22,7 +26,7 @@ export default function ChatPage() {
       chatname: "WoonHakki",
       last_message: "อุนักๆ อุนักๆๆ อุนนานัก",
       last_message_time: "2024-04-15 12:00",
-      unread: 0,
+      unread: 2,
       is_pinned: false,
       profile_picture: "1",
       is_group: false,
@@ -44,15 +48,25 @@ export default function ChatPage() {
   ];
   return (
     <main className="w-full min-h-[100vh] bg-cpc-blue">
-      {createGroup && (
+      {(createGroup || changeColor) && (
         <div
           className="absolute inset-0 bg-black bg-opacity-40 z-10"
           onClick={() => setCreateGroup(false)}
         />
       )}
       <div className="flex flex-row">
-        <ChatLists chatLists={chatLists} setCreateGroup={setCreateGroup} />
-        <Chat />
+        <img
+          src={Rings}
+          alt="rings"
+          className="absolute w-[6.2rem] translate-x-[32rem] translate-y-[10rem]"
+        />
+        <ChatLists
+          chatLists={chatLists}
+          setCreateGroup={setCreateGroup}
+          selectedChat={selectedChat}
+          setSelectedChat={setSelectedChat}
+        />
+        <Chat chatColor={chatColor} setChangeColor={setChangeColor} selectedChat={selectedChat} />
       </div>
     </main>
   );
--- frontend/src/pages/ChatPage/components/Chat.tsx
+++ frontend/src/pages/ChatPage/components/Chat.tsx
@@ -1,3 +1,43 @@
-export default function Chat() {
-  return <div></div>;
+export default function Chat({
+  chatColor,
+  setChangeColor,
+  selectedChat,
+}: {
+  chatColor: string;
+  setChangeColor: (changeColor: boolean) => void;
+  selectedChat: string;
+}) {
+  let bgColor = "bg-[#E2E7F7]";
+  let bgTopColor = "bg-cpc-orange";
+  switch (chatColor) {
+    case "orange":
+      bgColor = "bg-[#E2E7F7]";
+      bgTopColor = "bg-cpc-orange";
+      break;
+    case "pink":
+      bgColor = "bg-[#FFE6E9]";
+      bgTopColor = "bg-[#FF8897]";
+      break;
+    case "purple":
+      bgColor = "bg-[#E5DFFF]";
+      bgTopColor = "bg-[#A791FF]";
+      break;
+    case "green":
+      bgColor = "bg-[#E7F7E2]";
+      bgTopColor = "bg-[#A2D92E]";
+      break;
+    case "yellow":
+      bgColor = "bg-[#FFFDD6]";
+      bgTopColor = "bg-[#FFFA7C]";
+      break;
+  }
+  return (
+    <div
+      className={`w-[60%] h-[80vh] my-10 mr-16 ml-5 ${bgColor} rounded-md shadow-[inset_2px_2px_6px_0px_rgba(0,0,0,0.25)]`}
+    >
+      <div
+        className={`w-full h-[4.5rem] ${bgTopColor} rounded-t-md shadow-[inset_2px_2px_6px_0px_rgba(0,0,0,0.25)]`}
+      ></div>
+    </div>
+  );
 }
