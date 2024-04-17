a1a2933aed2258fc4268ff05a696b688e0f4e9ca
Thanach Silapatcharanun <111685309+Porsche-ths@users.noreply.github.com>
2024-04-18 01:44:28 +0700

    feat: use auth context provider

--- frontend/src/routes/getPageData.tsx
+++ frontend/src/routes/getPageData.tsx
@@ -4,26 +4,27 @@ import LoginPage from "../pages/LoginPage/LoginPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
 import SearchPage from "../pages/SearchPage/SearchPage";
 import ChatPage from "../pages/ChatPage/ChatPage";
+import { AuthProvider } from "../common/context/AuthContext";
 
 export const getPagesData = () =>
-  [ {
-      path: "/",
-      element: <PageRedirect to="/login" />,
-    },
-    {
-      path: "/login",
-      element: <LoginPage />,
-    },
-    {
-      path: "/register",
-      element: <RegisterPage />,
-    },
-    {
-      path: "/search",
-      element: <SearchPage />,
-    },
-    {
-      path: "/chat",
-      element: <ChatPage />,
-    },
-] as unknown as RouteProps[];
+  [{
+    path: "/",
+    element: <PageRedirect to="/login" />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "/search",
+    element: <AuthProvider><SearchPage /></AuthProvider>,
+  },
+  {
+    path: "/chat",
+    element: <AuthProvider><ChatPage /></AuthProvider>,
+  },
+  ] as unknown as RouteProps[];
