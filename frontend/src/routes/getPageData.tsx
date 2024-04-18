83b9ff0e3ed861426c94f3cbfb39b332da54bb97
its144prelationship <pastalonely@gmail.com>
2024-04-18 18:48:49 +0700

    feat: recovery auth provider

--- frontend/src/routes/getPageData.tsx
+++ frontend/src/routes/getPageData.tsx
@@ -21,10 +21,10 @@ export const getPagesData = () =>
   },
   {
     path: "/search",
-    element: <SearchPage />,
+    element: <AuthProvider><SearchPage /></AuthProvider>,
   },
   {
     path: "/chat",
-    element: <ChatPage />,
+    element: <AuthProvider><ChatPage /></AuthProvider>,
   },
   ] as unknown as RouteProps[];
