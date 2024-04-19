4d348e82be948aab25b9e85157d2db1705c6e6e2
Thanach Silapatcharanun <111685309+Porsche-ths@users.noreply.github.com>
2024-04-19 15:06:21 +0700

    fix: fix AuthContextProvider

--- frontend/src/common/context/AuthContext.tsx
+++ frontend/src/common/context/AuthContext.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { createContext } from "react";
 import { jwtDecode } from "jwt-decode";
 import { AuthContextType } from "../types/AuthContextType";
@@ -10,20 +11,57 @@ export const AuthContext = createContext<AuthContextType>({
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const token = LocalStorageUtils.getData("token");
-    if (!token) {
-        window.location.href = "/login";
-        return;
-    }
+    const [contextValue, setContextValue] = useState<AuthContextType>({
+        user_id: "",
+        username: "",
+        profile_picture: ""
+    });
 
-    try {
-        const decodedToken = jwtDecode<AuthContextType>(token);
-        const contextValue = { ...decodedToken };
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const token = LocalStorageUtils.getData("token");
+                if (!token) {
+                    window.location.href = "/login";
+                    return;
+                }
 
-        return (<AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>);
-    } catch (err) {
-        LocalStorageUtils.removeData("token");
-        window.location.href = "/login";
-        return;
-    }
-}
+                const decodedToken = jwtDecode<AuthContextType>(token);
+                if (!decodedToken) {
+                    LocalStorageUtils.removeData("token");
+                    window.location.href = "/login";
+                    return;
+                }
+
+                const response = await fetch(`http://localhost:5789/api/user/me`, {
+                    method: "GET",
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                });
+
+                const data = await response.json();
+                const user = data.data;
+
+                const authContextValue: AuthContextType = {
+                    user_id: user._id,
+                    username: user.username,
+                    profile_picture: user.profile_picture
+                };
+
+                setContextValue(authContextValue);
+            } catch (err) {
+                LocalStorageUtils.removeData("token");
+                window.location.href = "/login";
+            }
+        };
+
+        checkAuth();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
